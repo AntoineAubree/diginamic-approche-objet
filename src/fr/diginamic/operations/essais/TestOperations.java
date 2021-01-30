39e9c3f753f7ebcefdb55cf52ee46e5f863f3c2a
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-01-30 16:38:00 +0100

    add Operation and TestOperation

---
+++ src/fr/diginamic/operations/essais/TestOperations.java
@@ -0,0 +1,20 @@
+package fr.diginamic.operations.essais;
+
+import fr.diginamic.operations.Operations;
+
+public class TestOperations {
+
+	public static void main(String[] args) {
+		
+		double resultatAddition = Operations.calcul(150, 50, '+');
+		double resultatSoustraction = Operations.calcul(150, 50, '-');
+		double resultatMutliplication = Operations.calcul(150, 50, '*');
+		double resultatDivision = Operations.calcul(150, 50, '/');
+		
+		System.out.println("150 + 50 = " + resultatAddition);
+		System.out.println("150 - 50 = " + resultatSoustraction);
+		System.out.println("150 * 50 = " + resultatMutliplication);
+		System.out.println("150 / 50 = " + resultatDivision);
+	}
+
+}
